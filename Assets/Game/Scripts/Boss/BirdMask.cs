57dd29d477b13b20e65e202275a1fcae92989241
joesibarani2000 <joesibarani2000@gmail.com>
2020-10-31 00:11:12 +0700

    boss move

---
+++ Assets/Game/Scripts/Boss/BirdMask.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdMask : BossBehaviour
+{
+    enum State_BirdMask
+    {
+        IDLE,
+        MACHINEGUN,
+        MISSILE,
+        SWIPE
+    }
+
+    private System.Enum currentState;
+    private System.Enum [] stateSequences;
+    private int stateIndex;
+
+    private void UpdateState()
+    {
+        switch (currentState)
+        {
+            case BossState.PREPARATION:
+                break;
+            case State_BirdMask.IDLE:
+                Idle();
+                break;
+            case State_BirdMask.MISSILE:
+                Missile();
+                break;
+            case State_BirdMask.MACHINEGUN:
+                MachineGun();
+                break;
+            case State_BirdMask.SWIPE:
+                Swipe();
+                break;
+            case BossState.FINAL:
+                stateIndex = 0;
+                break;
+        }
+    }
+
+
+
+    private void NextState()
+    {
+        stateIndex++;
+        currentState = stateSequences[stateIndex];
+    }
+
+    private void OnEnterIdle()
+    {
+
+    }
+
+    private void OnExitIdle()
+    {
+
+    }
+
+
+    private void Idle()
+    {
+        OnEnterIdle();
+        OnExitIdle();
+    }
+
+    private void OnEnterMissile()
+    {
+
+    }
+
+    private void OnExitMissile()
+    {
+
+    }
+
+    private void Missile()
+    {
+        OnEnterMissile();
+        OnExitMissile();
+    }
+    private void OnEnterMachineGun()
+    {
+
+    }
+
+    private void OnExitMachineGun()
+    {
+
+    }
+
+    private void MachineGun()
+    {
+        OnEnterMachineGun();
+        OnExitMachineGun();
+    }
+
+    private void OnEnterSwipe()
+    {
+
+    }
+
+    private void OnExitSwipe()
+    {
+
+    }
+
+    private void Swipe()
+    {
+        OnEnterSwipe();
+        OnExitSwipe();
+    }
+
+}
+
+
+
---
+++ Assets/Game/Scripts/Boss/BossBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BossBehaviour : MonoBehaviour
+{
+    protected int health;
+    protected AttackPattern[] patterns;
+
+    protected enum BossState
+    {
+        PREPARATION,
+        FINAL
+    }
+
+}
+
+[System.Serializable] 
+public class AttackPattern
+{
+    public string attackName;
+
+    public UnityAction attackEvent; 
+}
