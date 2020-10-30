9acaf232fffd330c97b57436020f4d0e0ce74d48
Ivan Lianto <ivanlianto59@gmail.com>
2020-10-30 23:03:51 +0700

    Inventory

---
+++ Assets/Game/Scripts/Inventory/InventorySlot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class InventorySlot : MonoBehaviour
+{
+    [Header("UI Stuff to change")]
+    [SerializeField] private Text itemNumberText;
+    [SerializeField] private Image itemImage;
+
+    [Header("Variable from the text")]
+    public InventoryItem thisItem;
+    public InventoryManager thisManager;
+
+    public void Setup(InventoryItem newItem, InventoryManager newManager)
+    {
+        thisItem = newItem;
+        thisManager = newManager;
+        if (thisItem)
+        {
+            itemImage.sprite = thisItem.itemSprite;
+            itemNumberText.text = "" + thisItem.numberHeld;
+        }
+    }
+
+    public void ClickedOn()
+    {
+        if (thisItem)
+        {
+            thisManager.SetupDescriptionAndButton(thisItem.itemDescription, thisItem.usable, thisItem);
+        }
+    }
+}
---
+++ Assets/Game/Scripts/Inventory/PhysicalItem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicalItem : MonoBehaviour
+{
+    [SerializeField] private PlayerInventory playerInventory;
+    [SerializeField] private InventoryItem thisItem;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            AddItemToInventory();
+            Destroy(this.gameObject);
+        }
+    }
+
+    void AddItemToInventory()
+    {
+        if (playerInventory && thisItem)
+        {
+            if (playerInventory.myInventory.Contains(thisItem))
+            {
+                thisItem.numberHeld += 1;
+            }
+            else
+            {
+                playerInventory.myInventory.Add(thisItem);
+                thisItem.numberHeld += 1;
+            }
+        }
+    }
+}
