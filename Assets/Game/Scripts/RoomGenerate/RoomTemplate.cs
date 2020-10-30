43ad42c8328c053954549ecfdcd6b09464b5cf29
Robertus Dwi Ari Utomo <robertus.ari.utomo@gmail.com>
2020-10-30 18:12:22 +0700

    Add some utils
    [Add] Adding my utils
    [Add] Adding room generate logic

---
+++ Assets/Game/Scripts/RoomGenerate/RoomTemplate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplate : MonoBehaviour
+{
+    [SerializeField] private Room[] rooms;
+
+    public Room GetRandomRoom()
+    {
+        int randomPick = Random.Range(0, rooms.Length);
+        return rooms[randomPick];
+    }
+}
