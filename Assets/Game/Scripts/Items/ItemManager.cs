ae6f8efd955e440cea902d761fb6e382aff47829
Robertus Dwi Ari Utomo <robertus.ari.utomo@gmail.com>
2020-11-01 18:53:32 +0700

    Update item logic

--- Assets/Game/Scripts/Items/ItemManager.cs
+++ Assets/Game/Scripts/Items/ItemManager.cs
@@ -1,19 +1,30 @@
 ﻿using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemManager : MonoBehaviour
 {
     [SerializeField] private ItemData[] items;
 
     private static ItemData activeItem;
+    [SerializeField] private Button buttonActive;
+
+    private void Start()
+    {
+        if (buttonActive) buttonActive.interactable = activeItem != null;
+    }
 
     public void ChooseItem(string itemName)
     {
-        if (activeItem) activeItem = items.First(e => e.itemName == itemName);
+        activeItem = items.First(e => e.itemName == itemName);
     }
 
     public void ActivateItem()
     {
-        activeItem.TakeEffect();
+        if (activeItem)
+        {
+            activeItem.TakeEffect();
+            if (activeItem.CheckIsOneTimeUse()) buttonActive.interactable = false;
+        }
     }
 }
