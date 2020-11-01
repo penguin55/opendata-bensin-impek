1c32156757fd64069df45a169f2d03b38ac26fd7
Ivan Lianto <ivanlianto59@gmail.com>
2020-11-01 19:15:55 +0700

    Update

---
+++ Assets/Game/Scripts/UI/TobecontinudUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TomWill;
+public class TobecontinudUI : MonoBehaviour
+{
+    void Start()
+    {
+        TWTransition.FadeOut();
+    }
+
+    void Update()
+    {
+        End();
+    }
+
+    public void End()
+    {
+        StartCoroutine(ToMainMenu());
+    }
+
+    IEnumerator ToMainMenu()
+    {
+        yield return new WaitForSeconds((float)TimelineManager.instance.Director.duration);
+        TWTransition.FadeIn(() => TWLoading.LoadScene("MainMenu"));
+    }
+}
