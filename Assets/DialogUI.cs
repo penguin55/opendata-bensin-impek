dea3e3b2f7f52b01dae2b6c045db639229f2ec8c
joesibarani2000 <joesibarani2000@gmail.com>
2020-11-01 08:44:06 +0700

    Dialog New Scene
    setelah lihat script bintang keknya dialogi dibkin satu scene berbeda aja

---
+++ Assets/DialogUI.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TomWill;
+
+public class DialogUI : MonoBehaviour
+{
+    [SerializeField] private GameObject dialogUI;
+    [SerializeField] [TextArea] private string[] chat;
+    [SerializeField] private Text dialog;
+    [SerializeField] private int index = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TWLoading.OnSuccessLoad(() => {
+            TWTransition.FadeOut();
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Dialog();
+    }
+
+    public void next()
+    {
+        index++;
+    }
+
+    public void Dialog()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TWTransition.FadeIn(() => TWLoading.LoadScene("BossTest"));
+        }
+        if (index < chat.Length) dialog.text = chat[index];
+        if (index >= chat.Length)
+        {
+            dialogUI.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+}
--- Assets/InGameUI.cs
+++ Assets/InGameUI.cs
@@ -7,13 +7,11 @@ using UnityEngine.UI;
 public class InGameUI : MonoBehaviour
 {
     private bool isPaused;
-    [SerializeField] private GameObject pauseMenuUI, DialogUI;
+    [SerializeField] private GameObject pauseMenuUI;
     [SerializeField] private GameObject[] hearts;
     [SerializeField] private Sprite newsprite;
     [SerializeField] private Sprite oldsprite;
-    [SerializeField] [TextArea] private string[] chat;
-    [SerializeField] private Text dialog;
-    [SerializeField] private int index = 0;
+    
 
     public static InGameUI instance;
 
@@ -24,35 +22,15 @@ public class InGameUI : MonoBehaviour
         TWLoading.OnSuccessLoad(() => {
             TWTransition.FadeOut();
         });
-        Time.timeScale = 0f;
-        DialogUI.SetActive(true);
         uilive();
     }
 
-    public void next()
-    {
-        index++;
-    }
-
-    public void Dialog()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            DialogUI.SetActive(false);
-            Time.timeScale = 1f;
-        }
-        if (index < chat.Length) dialog.text = chat[index];
-        if (index >= chat.Length)
-        {
-            DialogUI.SetActive(false);
-            Time.timeScale = 1f;
-        }
-    }
+  
 
     // Update is called once per frame
     void Update()
     {
-        Dialog();
+        
         OpenPauseMenu();
     }
 
