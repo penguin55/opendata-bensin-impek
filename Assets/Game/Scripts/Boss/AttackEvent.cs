c3adc4ac2856485ebbc2c7e05c108cc394635970
Ivan Lianto <ivanlianto59@gmail.com>
2020-11-14 13:15:10 +0700

    Nier automata
    Add blink boss

--- Assets/Game/Scripts/Boss/AttackEvent.cs
+++ Assets/Game/Scripts/Boss/AttackEvent.cs
@@ -4,14 +4,25 @@ using UnityEngine.Events;
 
 public class AttackEvent : MonoBehaviour
 {
-    [SerializeField] protected float delay_enter, delay_active, delay_exit;
+    [SerializeField] protected float delay_enter, delay_prepare, delay_active, delay_exit;
+    [SerializeField] protected Animator anim;
 
     protected UnityAction onCompleteAction;
 
     public virtual void ExecutePattern(UnityAction onComplete)
     {
         onCompleteAction = onComplete;
-        DOVirtual.DelayedCall(delay_enter, OnEnter_Attack);
+        DOVirtual.DelayedCall(delay_enter, OnPrepare_Attack);
+    }
+
+    protected virtual void OnPrepare_Attack()
+    {
+        anim.gameObject.SetActive(true);
+        DOVirtual.DelayedCall(delay_prepare, () =>
+        {
+            anim.gameObject.SetActive(false);
+            OnEnter_Attack();
+        });
     }
 
     protected virtual void OnEnter_Attack()
