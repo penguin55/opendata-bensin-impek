24481a2d7dc222952d08e25ef4bddecf9a4acc68
Ivan Lianto <ivanlianto59@gmail.com>
2020-11-16 18:29:33 +0700

    Foot step sound

---
+++ Assets/Game/Scripts/Player/PlayerSound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TomWill;
+public class PlayerSound : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FootStep()
+    {
+        TWAudioController.PlaySFX("PLAYER_SFX", "player_move");
+    }
+}
