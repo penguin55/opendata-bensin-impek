5bfbb3240f9947b34ebb4ec257b8faf47699a04d
joesibarani2000 <joesibarani2000@gmail.com>
2020-11-18 22:14:32 +0700

    update fungus

---
+++ Assets/Game/Scripts/Utils/Fungus/FungusController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class FungusController : MonoBehaviour
+{
+    [SerializeField] private Flowchart flowchart;
+    [SerializeField] DialogInput dialogInput;
+
+    public void Init()
+    {
+        flowchart.ExecuteBlock("MissionStart");
+    }
+
+    public void NextBlock(string blockname)
+    {
+        flowchart.ExecuteBlock(blockname);
+    }
+
+    public void NextDialog()
+    {
+        dialogInput.SetButtonClickedFlag();
+    }
+}
