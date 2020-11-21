e964667a9e93c84f3081fe4369916bea853db5dc
joesibarani2000 <joesibarani2000@gmail.com>
2020-11-21 20:38:17 +0700

    roundflame state gatekeeper

--- Assets/Game/Scripts/Boss/GateKeeper/AttackPattern/GateKeeper_RoundFlame.cs
+++ Assets/Game/Scripts/Boss/GateKeeper/AttackPattern/GateKeeper_RoundFlame.cs
@@ -5,6 +5,17 @@ using UnityEngine.Events;
 
 public class GateKeeper_RoundFlame : AttackEvent
 {
+    [SerializeField] private GameObject laserParent;
+    [SerializeField] private Transform damageArea;
+    [SerializeField] private Transform target;
+    [SerializeField] private Vector2 offset;
+    [SerializeField] private float timeToMove;
+    [SerializeField] private Vector2 beginning, end;
+
+    [SerializeField] private ParticleSystem fires;
+
+    [SerializeField] private Animator attack;
+
     public override void ExecutePattern(UnityAction onComplete)
     {
         base.ExecutePattern(onComplete);
@@ -12,16 +23,49 @@ public class GateKeeper_RoundFlame : AttackEvent
 
     protected override void OnEnter_Attack()
     {
+        laserParent.SetActive(true);
+        laserParent.transform.localScale = Vector2.Lerp(laserParent.transform.localScale, end, timeToMove * Time.deltaTime);
+        ActivateDamageArea(true);
         base.OnEnter_Attack();
     }
 
     protected override void Attack()
     {
-        base.Attack();
+        ActivateDamageEffect();
+        TWAudioController.PlaySFX("SFX_BOSS", "laser");
+
+        DOVirtual.DelayedCall(1, () => CameraShake.instance.Shake(1, 1, 2)).SetLoops(-1).SetId("ShakeLaser");
+
+        DOTween.Sequence()
+            .Append(damageArea.DOMove(target.position + Vector3.up * offset.y, timeToMove))
+            .OnComplete(() =>
+            {
+                DOTween.Kill("ShakeLaser");
+                base.Attack();
+            });
     }
 
     protected override void OnExit_Attack()
     {
+
+        ActivateDamageArea(false);
+        laserParent.transform.localScale = Vector2.Lerp(laserParent.transform.localScale,beginning, timeToMove * Time.deltaTime);
+        laserParent.SetActive(false);
         base.OnExit_Attack();
+    }
+
+    private void ActivateDamageArea(bool active)
+    {
+            damageArea.gameObject.SetActive(active);
+            damageArea.GetComponent<Collider2D>().enabled = false;
+     }
+    
+
+    private void ActivateDamageEffect()
+    {
+
+        fires.Play();
+        damageArea.GetComponent<Collider2D>().enabled = true;
+        
     }
 }
