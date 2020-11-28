0a9347fad2d4e022e84cd07a0e846cc5f929d73f
joesibarani2000 <joesibarani2000@gmail.com>
2020-11-28 14:08:40 +0700

    update timeline

--- Assets/Game/Scripts/Timeline/TimelineManager.cs
+++ Assets/Game/Scripts/Timeline/TimelineManager.cs
@@ -8,15 +8,9 @@ public class TimelineManager : MonoBehaviour
     public static TimelineManager instance;
     [SerializeField] private Animator playerAnimator;
     [SerializeField] private RuntimeAnimatorController runtime;
-    [SerializeField] private PlayableDirector director;
-
-    [SerializeField] private PlayableAsset terracopter;
-    [SerializeField] private PlayableAsset gatekeeper;
-    [SerializeField] private PlayableAsset headhunter;
-    [SerializeField] private PlayableAsset unholychariot;
-
-
-    public PlayableDirector Director { get => director; set => director = value; }
+    [SerializeField] private PlayableDirector terrorcopter, gatekeeper, headhunter, unholyChariot;
+    private PlayableDirector currentDirector;
+    public PlayableDirector Director { get => currentDirector; set => currentDirector = value; }
 
     void Start()
     {
@@ -29,22 +23,23 @@ public class TimelineManager : MonoBehaviour
         switch (GameData.ActiveBoss)
         {
             case GameData.BossType.TERRORCOPTER:
-                director.Play(gatekeeper);
+                currentDirector = terrorcopter;
                 break;
             case GameData.BossType.GATEKEEPER:
-                director.Play(gatekeeper);
+                currentDirector = gatekeeper;
                 break;
             case GameData.BossType.UNHOLYCHARIOT:
-                director.Play(unholychariot);
+                currentDirector = unholyChariot;
                 break;
             case GameData.BossType.HEADHUNTER:
-                director.Play(headhunter);
+                currentDirector = headhunter;
                 break;
-        }   
+        }
+        currentDirector.Play();
     }
 
     public void StopDirector()
     {
-        director.Stop();
+        currentDirector.Stop();
     }
 }
