b0a142eb1a0b3d298ecb1c1120f64aefcf10d74a
Robertus Dwi Ari Utomo <robertus.ari.utomo@gmail.com>
2020-12-20 16:36:26 +0700

    Update Basic Training

--- Assets/Game/Scripts/Training/ListenerEvent/TLE_Dash.cs
+++ Assets/Game/Scripts/Training/ListenerEvent/TLE_Dash.cs
@@ -1,18 +1,73 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using DG.Tweening;
 using UnityEngine;
 
-public class TLE_Dash : MonoBehaviour
+public class TLE_Dash : TrainingListenerEvent
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject parent;
+    [SerializeField] private GameObject damageArea;
+    [SerializeField] private float startTime, moveTime;
+    [SerializeField] private Transform[] positionsMove;
+    [SerializeField] private Transform[] playerPositions;
+
+    private bool dash;
+    private Transform start, target;
+
+    private bool moveRight;
+
+    public override void ActivateEventListener(bool flag)
     {
-        
+        base.ActivateEventListener(flag);
+
+        parent.SetActive(flag);
+
+        if (flag)
+        {
+            if (moveRight)
+            {
+                start = positionsMove[0];
+                target = positionsMove[positionsMove.Length - 1];
+                CharaController.instance.transform.position = playerPositions[positionsMove.Length - 1].position;
+            }
+            else
+            {
+                start = positionsMove[positionsMove.Length - 1];
+                target = positionsMove[0];
+                CharaController.instance.transform.position = playerPositions[0].position;
+            }
+
+            damageArea.transform.DOMove(start.position, 0f);
+
+            DOVirtual.DelayedCall(startTime, ()=> MoveDamage());
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void InitEventListener<E>(E param)
     {
-        
+        if (typeof(bool).Equals(typeof(E)))
+        {
+            moveRight = ((bool)(object)param);
+        }
+    }
+
+    protected override bool ValidateEventListener<E>(E eventListener)
+    {
+        return base.ValidateEventListener(eventListener);
+    }
+
+    protected override void CompleteEventListener<E>(ref E eventListener)
+    {
+        base.CompleteEventListener(ref eventListener);
+        eventListener = (E)System.Convert.ChangeType(true, typeof(E));
+
+        manager.CompleteTrainingSection();
+    }
+
+    private void MoveDamage()
+    {
+        damageArea.transform.DOMove(target.position, moveTime).SetEase(Ease.Linear)
+            .OnComplete(()=>
+            {
+                CompleteEventListener(ref dash);
+            });
     }
 }
