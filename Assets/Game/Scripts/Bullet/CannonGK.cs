b76eb27d005fa58d25541824ab1ac10218e610f9
joesibarani2000 <joesibarani2000@gmail.com>
2020-12-22 16:15:43 +0700

    update smoke trail sfx for canon

--- Assets/Game/Scripts/Bullet/CannonGK.cs
+++ Assets/Game/Scripts/Bullet/CannonGK.cs
@@ -7,13 +7,14 @@ public class CannonGK : DamageArea
     private Vector3 direction;
     private float speed;
     private bool launch;
-
+    [SerializeField]private ParticleSystem smoketrail, explode;
     public void Launch(Vector3 direction, float speed)
     {
         this.direction = direction;
         this.speed = speed;
         transform.localEulerAngles = new Vector3(0f, 0f, GetAngle(direction));
         DOVirtual.DelayedCall(3, () => { OnExit_State(); }).SetId("Cannon"+transform.GetInstanceID());
+        smoketrail.Play();
         OnEnter_State();
     }
 
@@ -36,20 +37,20 @@ public class CannonGK : DamageArea
 
     protected override void OnExit_State()
     {
-        
+        smoketrail.Stop();
         base.OnExit_State();
     }
 
     public void Explode()
     {
+        smoketrail.Stop();
         this.GetComponent<SpriteRenderer>().enabled = false;
-        ParticleSystem particle = this.GetComponent<ParticleSystem>();
-        particle.Play();
+        explode.Play();
 
         TWAudioController.PlaySFX("SFX_BOSS", "tank_projectiles");
         CameraShake.instance.Shake(1, 3, 5);
 
-        DOVirtual.DelayedCall(particle.main.startLifetimeMultiplier, () => {
+        DOVirtual.DelayedCall(explode.main.startLifetimeMultiplier, () => {
             Destroy(this);
         });
     }
