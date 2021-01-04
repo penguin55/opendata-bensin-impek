134622c1629be43653f871c4d194ff3036f4e922
joesibarani2000 <joesibarani2000@gmail.com>
2021-01-04 20:27:31 +0700

    preparing chariot attack pattern template

---
+++ Assets/Game/Scripts/Boss/Chariot/AttackPattern/Chariot_Machinegun.cs
@@ -0,0 +1,98 @@
+﻿using DG.Tweening;
+using TomWill;
+using UnityEngine;
+using UnityEngine.Events;
+public class Chariot_Machinegun : AttackEvent
+{
+    [SerializeField] private Vector2[] swipeAreaPosition;
+    [SerializeField] private Transform swipeObject;
+    [SerializeField] private bool moveToRight;
+    [SerializeField] private float timeToMove;
+    [SerializeField] private float timeGoBack;
+    [SerializeField] private GameObject birdMask;
+
+    [SerializeField] private Animator attack;
+
+    public override void ExecutePattern(UnityAction onComplete)
+    {
+        base.ExecutePattern(onComplete);
+    }
+
+    public void MoveToLeft()
+    {
+        moveToRight = false;
+    }
+
+    protected override void OnEnter_Attack()
+    {
+
+        swipeObject.gameObject.SetActive(true);
+        Vector2 helicopterOffset;
+        if (moveToRight)
+        {
+            helicopterOffset = new Vector2(swipeAreaPosition[0].x, birdMask.transform.position.y);
+            swipeObject.localPosition = swipeAreaPosition[0];
+            birdMask.transform.DORotate(Vector3.forward * 25, 0.5f);
+        }
+        else
+        {
+            helicopterOffset = new Vector2(swipeAreaPosition[1].x, birdMask.transform.position.y);
+            swipeObject.localPosition = swipeAreaPosition[1];
+            birdMask.transform.DORotate(Vector3.forward * -25, 0.5f);
+        }
+        birdMask.transform.DOMove(helicopterOffset, timeGoBack).OnComplete(() => { base.OnEnter_Attack(); });
+    }
+
+    protected override void Attack()
+    {
+        attack.SetBool("attack", true);
+        int indexToMove = moveToRight ? 1 : 0;
+        Vector2 helicopterOffset;
+        if (moveToRight)
+        {
+            TWAudioController.PlaySFX("BOSS_SFX", "helicopter_strafing");
+            helicopterOffset = new Vector2(swipeAreaPosition[1].x, birdMask.transform.position.y);
+            birdMask.transform.DORotate(Vector3.forward * -25, 0.5f);
+        }
+        else
+        {
+            TWAudioController.PlaySFX("BOSS_SFX", "helicopter_strafing");
+            helicopterOffset = new Vector2(swipeAreaPosition[0].x, birdMask.transform.position.y);
+            birdMask.transform.DORotate(Vector3.forward * 25, 0.5f);
+        }
+
+        Sound("machine_gun");
+        birdMask.transform.DOMove(helicopterOffset, timeToMove).SetEase(Ease.Linear);
+        swipeObject.DOLocalMove(swipeAreaPosition[indexToMove], timeToMove).SetEase(Ease.Linear).OnComplete(() =>
+        {
+            DOTween.Kill("MachineGun_Sound");
+            base.Attack();
+        });
+    }
+
+    private void Sound(string name)
+    {
+        float audioLength = TWAudioController.AudioLength(name, "SFX");
+        DOTween.Sequence()
+            .AppendCallback(() => TWAudioController.PlaySFX("SFX_BOSS", name))
+            .AppendCallback(() => CameraShake.instance.Shake(audioLength, 1, 2))
+            .PrependInterval(audioLength / 2)
+            .SetLoops(-1)
+            .SetId("MachineGun_Sound");
+    }
+
+    protected override void OnExit_Attack()
+    {
+        attack.SetBool("attack", false);
+        Vector2 helicopterOffset = new Vector2(0, birdMask.transform.position.y);
+
+        if (moveToRight) birdMask.transform.DORotate(Vector3.forward * 25, 0.5f);
+        else birdMask.transform.DORotate(Vector3.forward * -25, 0.5f);
+
+        birdMask.transform.DOMove(helicopterOffset, timeGoBack).SetEase(Ease.Linear).OnComplete(() => {
+            birdMask.transform.DORotate(Vector3.zero, 0.5f);
+            base.OnExit_Attack();
+        });
+        swipeObject.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Game/Scripts/Boss/Chariot/Chariot.cs
@@ -0,0 +1,198 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TomWill;
+using UnityEngine;
+using DG.Tweening;
+
+public class Chariot : BossBehaviour
+{
+    enum State_Chariot
+    {
+        PREPARATION,
+        FINAL,
+        DIE,
+        IDLE,
+        MACHINEGUN,
+        MISSILE,
+        POISONZONE,
+        SWIPE_TO_RIGHT,
+        SWIPE_TO_LEFT
+    }
+
+    private State_Chariot currentState;
+    [SerializeField] private State_Chariot [] stateSequences;
+    private int stateIndex;
+
+    private void Start()
+    {
+        Instance = this;
+        stateIndex = 0;
+        currentState = State_Chariot.PREPARATION;
+
+        Sprite = GetComponent<SpriteRenderer>();
+        Init();
+        
+        UpdateState();
+        InGameUI.instance.UpdateHpBos(health);
+    }
+
+
+    private void UpdateState()
+    {
+        switch (currentState)
+        {
+            case State_Chariot.PREPARATION:
+                Preparation();
+                break;
+            case State_Chariot.IDLE:
+                Idle();
+                break;
+            case State_Chariot.MISSILE:
+                Missile();
+                break;
+            case State_Chariot.POISONZONE:
+                PoisonZone();
+                break;
+            case State_Chariot.MACHINEGUN:
+                MachineGun();
+                break;
+            case State_Chariot.SWIPE_TO_RIGHT:
+                Swipe(true);
+                break;
+            case State_Chariot.SWIPE_TO_LEFT:
+                Swipe(false);
+                break;
+            case State_Chariot.FINAL:
+                Final();
+                break;
+            case State_Chariot.DIE:
+                Die();
+                break;
+        }
+    }
+
+
+
+    private void NextState()
+    {
+        if (!GameVariables.GAME_OVER)
+        {
+            stateIndex++;
+            currentState = stateSequences[stateIndex];
+            UpdateState();
+        }
+    }
+
+    protected override void Preparation()
+    {
+        base.Preparation();
+        NextState();
+    }
+
+    protected override void Die()
+    {
+        base.Die();
+    }
+
+    #region IDLE
+    private void OnEnterIdle()
+    {
+        currentAttackEvent = patterns.First(e => e.attackName == "Missile").attackEvent;
+    }
+
+    private void Idle()
+    {
+        OnEnterIdle();
+        currentAttackEvent.ExecutePattern(OnExitIdle);
+    }
+
+    private void OnExitIdle()
+    {
+        NextState();
+    }
+    #endregion
+
+    #region MISSILE
+    private void OnEnterMissile()
+    {
+        currentAttackEvent = patterns.First(e => e.attackName == "Missile").attackEvent;
+    }
+
+    private void Missile()
+    {
+        OnEnterMissile();
+
+        currentAttackEvent.ExecutePattern(OnExitMissile);
+    }
+
+    private void OnExitMissile()
+    {
+        NextState();
+    }
+    #endregion
+
+    #region MACHINEGUN
+    private void OnEnterMachineGun()
+    {
+        currentAttackEvent = patterns.First(e => e.attackName == "Machinegun").attackEvent;
+    }
+
+    private void MachineGun()
+    {
+        OnEnterMachineGun();
+        currentAttackEvent.ExecutePattern(OnExitMachineGun);
+    }
+
+    private void OnExitMachineGun()
+    {
+        NextState();
+    }
+    #endregion
+
+    #region SWIPE
+    private void OnEnterSwipe()
+    {
+        currentAttackEvent = patterns.First(e => e.attackName == "Swipe").attackEvent;
+    }
+
+    private void Swipe(bool moveRight)
+    {
+        OnEnterSwipe();
+        if (!moveRight) ((Chariot_Machinegun)currentAttackEvent).MoveToLeft();
+
+        currentAttackEvent.ExecutePattern(OnExitSwipe);
+    }
+
+    private void OnExitSwipe()
+    {
+        NextState();
+    }
+    #endregion
+
+    #region LASER
+    private void OnEnterPoisonZone()
+    {
+        currentAttackEvent = patterns.First(e => e.attackName == "Laser").attackEvent;
+    }
+
+    private void PoisonZone()
+    {
+        OnEnterPoisonZone();
+        currentAttackEvent.ExecutePattern(OnExitPoisonZone);
+    }
+
+    private void OnExitPoisonZone()
+    {
+        NextState();
+    }
+    #endregion
+
+    protected override void Final()
+    {
+        base.Final();
+
+        stateIndex = 1;
+        currentState = stateSequences[stateIndex];
+        UpdateState();
+    }
+}
