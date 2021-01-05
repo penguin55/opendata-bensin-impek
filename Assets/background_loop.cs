421f820c7c2109aec9eb811b4c00d8c926c12e02
joesibarani2000 <joesibarani2000@gmail.com>
2021-01-05 18:17:15 +0700

    moving background

---
+++ Assets/background_loop.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class background_loop : MonoBehaviour
+{
+    [SerializeField] private float scrollspeed;
+    private float offset;
+    private Material mat;
+    [SerializeField] private Renderer background;
+    [SerializeField] private string sortingLayer;
+    [SerializeField] private int sortingOrder;
+
+    private void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+        SetLayer();
+    }
+
+    private void Update()
+    {
+        offset += (Time.deltaTime * scrollspeed) / 10f;
+        mat.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+
+    public void SetLayer()
+    {
+        if (background == null)
+        {
+            background = this.GetComponent<Renderer>();
+        }
+
+        background.sortingLayerName = sortingLayer;
+        background.sortingOrder = sortingOrder;
+    }
+}
