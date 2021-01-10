775a8c7b2042bbef48b9e9461719ec68cea82962
joesibarani2000 <joesibarani2000@gmail.com>
2021-01-10 12:52:37 +0700

    Update Chariot_Missile.cs

--- Assets/Game/Scripts/Boss/Chariot/AttackPattern/Chariot_Missile.cs
+++ Assets/Game/Scripts/Boss/Chariot/AttackPattern/Chariot_Missile.cs
@@ -95,11 +95,9 @@ public class Chariot_Missile : AttackEvent
             }
         }
 
-        if (!activeMissile) deactiveMissileParent = spawnChoicePosition.GetComponent<MissileBM>();
-
         GameObject missile = Instantiate(projectilePrefabs, (spawnChoicePosition.position + Vector3.up * 30), Quaternion.identity, spawnChoicePosition);
         spawnChoicePosition.GetComponent<MissileBM>().Launch(missile, .1f, true);
 
-        if (!activeMissile) deactiveMissileProjectile = missile;
+        
     }
 }
