1b2d408f4db098e58f8932e1d8f47e358246db37
Ivan Lianto <ivanlianto59@gmail.com>
2021-01-10 15:35:13 +0700

    Update timeline

---
+++ Assets/Game/Scripts/Boss/Chariot/ChariotTimeline.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChariotTimeline : MonoBehaviour
+{
+    public string move;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        move = "start";
+    }
+
+    private void Update()
+    {
+        Chariot boss = BossBehaviour.Instance as Chariot;
+        boss.animationTweening.MoveTrain(move);
+    }
+
+    public void SetMoveName(string _move)
+    {
+        move = _move;
+    }
+}
