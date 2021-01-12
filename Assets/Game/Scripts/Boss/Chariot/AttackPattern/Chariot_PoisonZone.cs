281996bdab6f29220a4b845bc1badcf3554b9f2b
Ivan Lianto <ivanlianto59@gmail.com>
2021-01-12 21:09:14 +0700

    Poison

--- Assets/Game/Scripts/Boss/Chariot/AttackPattern/Chariot_PoisonZone.cs
+++ Assets/Game/Scripts/Boss/Chariot/AttackPattern/Chariot_PoisonZone.cs
@@ -7,9 +7,11 @@ using UnityEngine.Events;
 public class Chariot_PoisonZone : AttackEvent
 {
     [SerializeField] private GameObject poisonParent;
-    [SerializeField] private Transform damageArea;
+    [SerializeField] private Transform poisonPosition1;
+    [SerializeField] private Transform poisonPosition2;
     [SerializeField] private Transform[] poisonSpawn;
-    [SerializeField] private Collider2D collide;
+    [SerializeField] private Collider2D collide1;
+    [SerializeField] private Collider2D collide2;
 
     public override void ExecutePattern(UnityAction onComplete)
     {
@@ -29,13 +31,15 @@ public class Chariot_PoisonZone : AttackEvent
             .AppendCallback(()=> TWAudioController.PlaySFX("BOSS_SFX", "poison_gas"))
             .AppendCallback(() => poisonParent.SetActive(true))
             .AppendInterval(.5f)
-            .AppendCallback(() => collide.enabled = true);
+            .AppendCallback(() => collide1.enabled = true)
+            .AppendCallback(() => collide2.enabled = true);
         base.Attack();
     }
 
     protected override void OnExit_Attack()
     {
-        collide.enabled = false;
+        collide1.enabled = false;
+        collide2.enabled = false;
         poisonParent.SetActive(false);
         base.OnExit_Attack();
     }
@@ -45,13 +49,16 @@ public class Chariot_PoisonZone : AttackEvent
         switch (Chariot.Instance.health)
         {
             case 1:
-                damageArea.position = poisonSpawn[0].position;
+                poisonPosition1.position = poisonSpawn[0].position;
+                poisonPosition2.position = new Vector2(poisonSpawn[0].position.x, poisonPosition2.position.y);
                 break;
             case 2:
-                damageArea.position = poisonSpawn[1].position;
+                poisonPosition1.position = poisonSpawn[1].position;
+                poisonPosition2.position = new Vector2(poisonSpawn[1].position.x, poisonPosition2.position.y);
                 break;
             case 3:
-                damageArea.position = poisonSpawn[2].position;
+                poisonPosition1.position = poisonSpawn[2].position;
+                poisonPosition2.position = new Vector2(poisonSpawn[2].position.x, poisonPosition2.position.y);
                 break;
         }
     }
