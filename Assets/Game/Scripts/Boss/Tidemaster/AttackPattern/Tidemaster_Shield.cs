daa552325b6f5fab23d71d6a18e3653a7bd9720e
Robertus Dwi Ari Utomo <robertus.ari.utomo@gmail.com>
2021-01-16 15:05:38 +0700

    Update Shield pattern

--- Assets/Game/Scripts/Boss/Tidemaster/AttackPattern/Tidemaster_Shield.cs
+++ Assets/Game/Scripts/Boss/Tidemaster/AttackPattern/Tidemaster_Shield.cs
@@ -1,18 +1,144 @@
-﻿using System.Collections;
+﻿using DG.Tweening;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class Tidemaster_Shield : MonoBehaviour
+public class Tidemaster_Shield : AttackEvent
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float shieldTimer;
+    [SerializeField] private UIMiniGameTidemaster uiMiniGame;
+
+    private bool miniGameActive;
+    // Button status
+    private bool rightButtonIsActive;
+    private bool bottomButtonIsActive;
+    private bool leftButtonIsActive;
+    private bool topButtonIsActive;
+
+    private bool playerShipImmune;
+
+    protected override void OnPrepare_Attack()
     {
-        
+        base.OnPrepare_Attack();
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void OnEnter_Attack()
     {
-        
+        miniGameActive = true;
+        GameVariables.FREEZE_INPUT = true;
+
+        ActivateRandomButtonMiniGame();
+
+        DOVirtual.Float(0, shieldTimer, shieldTimer, (time)=>
+        {
+            OnInputMiniGame();
+            uiMiniGame.UpdateTimerInfo(shieldTimer, (shieldTimer - time));
+        }).SetEase(Ease.Linear).OnComplete(()=>
+        {
+            uiMiniGame.ActivateMiniGame(false);
+            miniGameActive = false;
+            GameVariables.FREEZE_INPUT = false;
+
+            playerShipImmune = CheckAllButtonActive();
+
+            base.OnEnter_Attack();
+        });
+
+        /*DOVirtual.DelayedCall(shieldTimer, ()=> {
+            uiMiniGame.ActivateMiniGame(false);
+            miniGameActive = false;
+            GameVariables.FREEZE_INPUT = false;
+
+            playerShipImmune = CheckAllButtonActive();
+
+            base.OnEnter_Attack();
+        }).OnUpdate(()=>
+        {
+            OnInputMiniGame();
+        });*/
+    }
+
+    protected override void Attack()
+    {
+        Debug.Log(playerShipImmune ? "Immune" : "Damage Taken");
+        base.Attack();
+    }
+
+    protected override void OnExit_Attack()
+    {
+        base.OnExit_Attack();
+    }
+
+    private void OnInputMiniGame()
+    {
+        if (Input.GetKey(InputManager.instance.moveRight))
+        {
+            rightButtonIsActive = true;
+            uiMiniGame.ActivateRightButton(true);
+        }
+
+        if (Input.GetKey(InputManager.instance.moveDown))
+        {
+            bottomButtonIsActive = true;
+            uiMiniGame.ActivateBottomButton(true);
+        }
+
+        if (Input.GetKey(InputManager.instance.moveLeft))
+        {
+            leftButtonIsActive = true;
+            uiMiniGame.ActivateLeftButton(true);
+        }
+
+        if (Input.GetKey(InputManager.instance.moveUp))
+        {
+            topButtonIsActive = true;
+            uiMiniGame.ActivateTopButton(true);
+        }
+    }
+
+    private void ActivateRandomButtonMiniGame()
+    {
+        bool deactiveFlag = false;
+
+        bool rightActive = Random.Range(1, 101) % 2 == 0;
+        bool bottomActive = Random.Range(1, 101) % 2 == 0;
+        bool leftActive = Random.Range(1, 101) % 2 == 0;
+        bool topActive = Random.Range(1, 101) % 2 == 0;
+
+        deactiveFlag = !(rightActive && bottomActive && leftActive && topActive);
+
+        if (!deactiveFlag)
+        {
+            float offset = Random.Range(0, 4);
+
+            switch (offset)
+            {
+                case 0:
+                    rightActive = false;
+                    break;
+                case 1:
+                    bottomActive = false;
+                    break;
+                case 2:
+                    leftActive = false;
+                    break;
+                case 3:
+                    topActive = false;
+                    break;
+            }
+        }
+
+        rightButtonIsActive = rightActive;
+        bottomButtonIsActive = bottomActive;
+        leftButtonIsActive = leftActive;
+        topButtonIsActive = topActive;
+
+        uiMiniGame.ActivateMiniGame(true, rightActive, bottomActive, leftActive, topActive);
+    }
+
+    private bool CheckAllButtonActive()
+    {
+        return rightButtonIsActive && bottomButtonIsActive && leftButtonIsActive && topButtonIsActive;
     }
 }
---
+++ Assets/Game/Scripts/UI/UIMiniGameTidemaster.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMiniGameTidemaster : MonoBehaviour
+{
+    [SerializeField] private GameObject miniGamePanel;
+    [SerializeField] private Image rightButtonActivated;
+    [SerializeField] private Image bottomButtonActivated;
+    [SerializeField] private Image leftButtonActivated;
+    [SerializeField] private Image topButtonActivated;
+    [SerializeField] private Text timerInfo;
+    [SerializeField] private Image timeFill;
+
+    public void ActivateMiniGame(bool flag, bool rightActivate = false, bool bottomActivate = false, bool leftActivate = false, bool topActivate = false)
+    {
+        miniGamePanel.SetActive(flag);
+
+        ActivateRightButton(rightActivate);
+        ActivateBottomButton(bottomActivate);
+        ActivateLeftButton(leftActivate);
+        ActivateTopButton(topActivate);
+    }
+
+    public void ActivateRightButton(bool flag)
+    {
+        if (flag) rightButtonActivated.color = Color.white;
+        else rightButtonActivated.color = Color.red;
+    }
+
+    public void ActivateBottomButton(bool flag)
+    {
+        if (flag) bottomButtonActivated.color = Color.white;
+        else bottomButtonActivated.color = Color.red;
+    }
+
+    public void ActivateLeftButton(bool flag)
+    {
+        if (flag) leftButtonActivated.color = Color.white;
+        else leftButtonActivated.color = Color.red;
+    }
+
+    public void ActivateTopButton(bool flag)
+    {
+        if (flag) topButtonActivated.color = Color.white;
+        else topButtonActivated.color = Color.red;
+    }
+
+    public void UpdateTimerInfo(float baseTime, float time)
+    {
+        //timerInfo.text = "Torpedo Launch in " + (int) time - 1 + " seconds";
+        //timeFill.fillAmount = time / baseTime;
+        float offset = time / baseTime; ;
+        Vector2 scaleFill = new Vector2(offset, 1f);
+        timeFill.rectTransform.localScale = scaleFill;
+    }
+}
