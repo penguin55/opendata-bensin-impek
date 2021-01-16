3fc3d37cae37fb9f7de76a4a88628e07ac8f571c
joesibarani2000 <joesibarani2000@gmail.com>
2021-01-16 10:41:26 +0700

    update template

--- Assets/Game/Scripts/Boss/Tidemaster/Tidemaster.cs
+++ Assets/Game/Scripts/Boss/Tidemaster/Tidemaster.cs
@@ -1,18 +1,172 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TomWill;
 using UnityEngine;
+using DG.Tweening;
 
-public class Tidemaster : MonoBehaviour
+public class Tidemaster : BossBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    enum State_Tidemaster
     {
-        
+        PREPARATION,
+        FINAL,
+        DIE,
+        IDLE,
+        TORPEDO_SHIELD,
+        MISSILE,
+        ENERGY_BALLS
     }
 
-    // Update is called once per frame
-    void Update()
+    private State_Tidemaster currentState;
+    [SerializeField] private State_Tidemaster[] stateSequences;
+    private int stateIndex;
+
+    private void Start()
     {
-        
+        Instance = this;
+        stateIndex = 0;
+        currentState = State_Tidemaster.PREPARATION;
+
+        Sprite = GetComponent<SpriteRenderer>();
+        Init();
+        UpdateState();
+        InGameUI.instance.UpdateHpBos(health);
+    }
+
+
+    private void UpdateState()
+    {
+        switch (currentState)
+        {
+            case State_Tidemaster.PREPARATION:
+                Preparation();
+                break;
+            case State_Tidemaster.IDLE:
+                Idle();
+                break;
+            case State_Tidemaster.MISSILE:
+                Missile();
+                break;
+            case State_Tidemaster.ENERGY_BALLS:
+                Energy_Balls();
+                break;
+            case State_Tidemaster.TORPEDO_SHIELD:
+                Torpedo_Shield();
+                break;
+            case State_Tidemaster.FINAL:
+                Final();
+                break;
+            case State_Tidemaster.DIE:
+                Die();
+                break;
+        }
+    }
+
+
+
+    private void NextState()
+    {
+        if (!GameVariables.GAME_OVER)
+        {
+            stateIndex++;
+            currentState = stateSequences[stateIndex];
+            UpdateState();
+        }
+    }
+
+    protected override void Preparation()
+    {
+        base.Preparation();
+        NextState();
+    }
+
+    protected override void Die()
+    {
+        base.Die();
+    }
+
+    #region IDLE
+    private void OnEnterIdle()
+    {
+        currentAttackEvent = patterns.First(e => e.attackName == "Missile").attackEvent;
+    }
+
+    private void Idle()
+    {
+        OnEnterIdle();
+        currentAttackEvent.ExecutePattern(OnExitIdle);
+    }
+
+    private void OnExitIdle()
+    {
+        NextState();
+    }
+    #endregion
+
+    #region MISSILE
+    private void OnEnterMissile()
+    {
+        currentAttackEvent = patterns.First(e => e.attackName == "Missile").attackEvent;
+    }
+
+    private void Missile()
+    {
+        OnEnterMissile();
+
+        currentAttackEvent.ExecutePattern(OnExitMissile);
+    }
+
+    private void OnExitMissile()
+    {
+        NextState();
+    }
+    #endregion
+
+    #region MACHINEGUN
+    private void OnEnterTorpedo_Shield()
+    {
+        currentAttackEvent = patterns.First(e => e.attackName == "Torpedo").attackEvent;
+    }
+
+    private void Torpedo_Shield()
+    {
+        OnEnterTorpedo_Shield();
+        currentAttackEvent.ExecutePattern(OnExitTorpedo_Shield);
+    }
+
+    private void OnExitTorpedo_Shield()
+    {
+        NextState();
+    }
+    #endregion
+
+    #region LASER
+    private void OnEnterEnergy_Balls()
+    {
+        currentAttackEvent = patterns.First(e => e.attackName == "Energy Balls").attackEvent;
+    }
+
+    private void Energy_Balls()
+    {
+        OnEnterEnergy_Balls();
+
+        currentAttackEvent.ExecutePattern(OnExitEnergy_Balls);
+    }
+
+    private void OnExitEnergy_Balls()
+    {
+        NextState();
+    }
+    #endregion
+
+    protected override void Final()
+    {
+        base.Final();
+
+        stateIndex = 1;
+        currentState = stateSequences[stateIndex];
+        UpdateState();
     }
 }
