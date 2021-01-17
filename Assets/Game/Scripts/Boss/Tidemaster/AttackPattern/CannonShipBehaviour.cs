db929f1ceca089b941c63fe1971abcbe90cd1f35
Robertus Dwi Ari Utomo <robertus.ari.utomo@gmail.com>
2021-01-17 16:37:08 +0700

    Placeholder dummy scene

---
+++ Assets/Game/Scripts/Boss/Tidemaster/AttackPattern/CannonShipBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonShipBehaviour : MonoBehaviour
+{
+    [SerializeField] private GameObject prefabBullet;
+    [SerializeField] private GameObject interactSign;
+    [SerializeField] private ParticleSystem particleShoot;
+    [SerializeField] private Animator cannonAnim;
+
+    private bool canInteract;
+
+    public void Activate(bool flag)
+    {
+        interactSign.SetActive(flag);
+        canInteract = flag;
+    }
+
+    protected void Launch()
+    {
+        if (canInteract)
+        {
+
+        }
+    }
+}
