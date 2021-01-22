e0189e752697d8442a4189f4babaf4ad7fbc8720
Ivan Lianto <ivanlianto59@gmail.com>
2021-01-22 23:42:34 +0700

    Update sprite Energy bools

--- Assets/Game/Scripts/Boss/Tidemaster/EnergyBalssTM.cs
+++ Assets/Game/Scripts/Boss/Tidemaster/EnergyBalssTM.cs
@@ -7,6 +7,8 @@ public class EnergyBalssTM : DamageArea
     [SerializeField] private float projectileTimeToMove;
     private Collider2D collider;  
     private GameObject projectile;
+    private Animator anim;
+    [SerializeField] private AnimationClip explodeClip;
     [SerializeField] private float duration;
     [SerializeField] private float strength;
     [SerializeField] private int vibrato;
@@ -18,6 +20,7 @@ public class EnergyBalssTM : DamageArea
         ParticleSystem smoke = projectile.transform.GetChild(2).GetComponent<ParticleSystem>();
         smoke.Play();
         deactiveMissileDashed = false;
+        anim = projectile.GetComponent<Animator>();
         collider = GetComponent<Collider2D>();
         collider.enabled = false;
         this.projectile = projectile;
@@ -42,16 +45,18 @@ public class EnergyBalssTM : DamageArea
     {
         base.OnExit_State();
 
+        anim.SetTrigger("Explode");
+
         DOVirtual.DelayedCall(0.2f, () =>
         {
             collider.enabled = false;
         });
-        projectile.GetComponent<SpriteRenderer>().enabled = false;
-        ParticleSystem particle = projectile.transform.GetChild(0).GetComponent<ParticleSystem>();
-        particle.Play();
+        //projectile.GetComponent<SpriteRenderer>().enabled = false;
+        //ParticleSystem particle = projectile.transform.GetChild(0).GetComponent<ParticleSystem>();
+        //particle.Play();
         TWAudioController.PlaySFX("SFX_BOSS", "energyball_impact");
         CameraShake.instance.Shake(duration, strength, vibrato);
-        DOVirtual.DelayedCall(particle.main.startLifetimeMultiplier, () =>
+        DOVirtual.DelayedCall(explodeClip.length, () =>
         {
             Destroy(projectile);
         });
