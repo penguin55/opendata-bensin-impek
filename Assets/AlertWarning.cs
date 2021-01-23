34010c0dacac5799ac0b3a27374a75600df502e1
joesibarani2000 <joesibarani2000@gmail.com>
2021-01-23 23:56:19 +0700

    complete ending scene

---
+++ Assets/AlertWarning.cs
@@ -0,0 +1,27 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AlertWarning : MonoBehaviour
+{
+    [SerializeField] private Image warningPanel;
+
+    public void ActiveAlert(bool flag)
+    {
+        if (flag)
+        {
+            DOTween.Sequence()
+                .Append(warningPanel.DOFade(.5f, 1))
+                .Append(warningPanel.DOFade(0, 1))
+                .SetLoops(-1, LoopType.Restart)
+                .SetId("warningAlert");
+        }
+        else
+        {
+            DOTween.Kill("warningAlert");
+            warningPanel.DOFade(0, 1);
+        }
+    }
+}
