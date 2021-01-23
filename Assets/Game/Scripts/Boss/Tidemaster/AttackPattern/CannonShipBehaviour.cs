037d35c512c5f38e1f4da3ac8c0b4c189d9b246d
joesibarani2000 <joesibarani2000@gmail.com>
2021-01-23 15:20:14 +0700

    update

--- Assets/Game/Scripts/Boss/Tidemaster/AttackPattern/CannonShipBehaviour.cs
+++ Assets/Game/Scripts/Boss/Tidemaster/AttackPattern/CannonShipBehaviour.cs
@@ -8,6 +8,7 @@ public class CannonShipBehaviour : MonoBehaviour
     [SerializeField] private ParticleSystem particleShoot;
     [SerializeField] private Animator cannonAnim;
     [SerializeField] private float modifierScaling;
+    [SerializeField] private Vector2 movePosition;
 
     private Vector2 baseScale;
     private bool canInteract;
@@ -38,7 +39,9 @@ public class CannonShipBehaviour : MonoBehaviour
             canInteract = false;
             interactSign.SetActive(canInteract);
             transform.DOPunchScale(baseScale * modifierScaling, 0.5f, 1);
+            transform.DOPunchPosition(movePosition, .5f, 1);
             TWAudioController.PlaySFX("BOSS_SFX", "cannon_launch");
+            particleShoot.Play();
             DOVirtual.DelayedCall(2, ()=> {
                 TWAudioController.PlaySFX("BOSS_SFX", "cannon_impact");
                 BossBehaviour.Instance.TakeDamage();
